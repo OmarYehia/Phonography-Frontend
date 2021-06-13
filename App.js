bd37fa20441f88c6c0f5c286cacf71bcec13f0cf
Yehia Khalil <yehiakhalilykk@gmail.com>
2021-06-13 17:45:06 +0200

    added post creation form

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Test from './components/test'
+import PostForm from './components/Post/addPostForm'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Test />
       <StatusBar style="auto" />
+      <PostForm />
     </View>
   );
 }
